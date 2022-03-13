4626b41e6d64559e4a234729656766ccd8332190
Marco Zocca <marco.zocca@unfoldml.com>
2022-03-13 15:19:33 +0100

    cross-check with 'ad'

--- src/Numeric/ODE/Symplectic.hs
+++ src/Numeric/ODE/Symplectic.hs
@@ -7,6 +7,7 @@ module Numeric.ODE.Symplectic where
 
 import GHC.List (iterate')
 import Numeric.AD.DelCont (AD', grad)
+import qualified Numeric.AD as AD (grad)
 
 data V a = V a a deriving (Eq, Show, Functor, Foldable, Traversable)
 instance Applicative V where
@@ -36,27 +37,42 @@ data H a = H (P a) (Q a) deriving (Eq, Show, Functor, Foldable, Traversable)
 
 -- https://courses.seas.harvard.edu/courses/am225/notes/am225_symplectic.pdf
 hamiltonian :: Floating a => H a -> a
-hamiltonian (H p q) = normSq p / 2 - (1 / norm q)
+hamiltonian (H p q) = tt p + uu q --  normSq p / 2 - (1 / norm q)
 
--- dHdt :: Floating a => H a -> (V a, V a)
--- dHdt h = (- dq, dp)
---   where
---     (H dp dq) = dH h
+tt :: Floating a => V a -> a
+tt p = normSq p / 2
+uu :: Floating a => V a -> a
+uu q = - 1 / norm q
 
+-- | Gradient of the Hamiltonian
 dH :: Floating b => H b -> H b
 dH = snd . grad hamiltonian
+
+dHa :: Floating a => H a -> H a
+dHa = AD.grad hamiltonian
+
+-- analytical derivatives of the hamiltonian
+-- dHdp :: H a -> V a
+-- dHdp (H p _) = p
+
+-- dHdq :: Floating b => H b -> V b
+dHdq :: Floating b => V b -> V b
+dHdq q = fmap f q
+  where
+    f s = s / den
+    den = norm q ** 3
 
 kepler :: [H Double]
 kepler = steps n h hh0
   where
     n = 10
-    h = pi / 240
+    h = 0.001 -- pi / 240
 
 hh0 :: H Double
 hh0 = H p0 q0
   where
-    p0 = V 0.001 (recip $ sqrt 2)
-    q0 = V (4/3) 0.001
+    p0 = V 0 (recip $ sqrt 2)
+    q0 = V (4/3) 0
 
 steps :: Floating a => Int -> a -> H a -> [H a]
 steps n h = take n . iterate (step h)
@@ -65,36 +81,37 @@ steps n h = take n . iterate (step h)
 step :: Floating a => a -> H a -> H a
 step h h0@(H p0 q0) = H p1 q1
   where
-    (H _ q') = dH h0
-    p1 = p0 - pure h * q'
+    (H _ q') = dH h0 -- gradient of current H.
+    p1 = p0 - pure h * q' -- update P
     h1 = H p1 q0 -- half-step updated Hamiltonian
-    (H p'' _) = dH h1
-    q1 = q0 + pure h * p''
+    (H p'' _) = dH h1 -- gradient of updated H.
+    q1 = q0 + pure h * p'' -- update Q
 
 stepIO :: (Show a, Floating a) => a -> H a -> IO (H a)
 stepIO h h0@(H p0 q0) = do
   let
-    dh1@(H _ q') = dH h0
-    p1 = p0 - pure h * q'
+    (H _ dUdq) = dHa h0
+    p1 = p0 - pure h * dUdq
     h1 = H p1 q0 -- half-step updated Hamiltonian
-    dh2@(H p'' _) = dH h1
-    q1 = q0 + pure h * p''
+    (H dTdp _) = dHa h1
+    q1 = q0 + pure h * dTdp
     h2 = H p1 q1
-  say "h0" h0
+  say "H(p, q) = T(p) + U(q)" h0
   -- say "dh1" dh1
-  say "q'" q'
-  say "p1" p1
-  say "h1" h1
-  say "dh2 = dH h1" dh2
+  say "dU/dq | H = H_0" dUdq
+  say "p_1/2" p1
+  say "H_1/2" h1
+  -- say "dh2 = dH h1" dh2
+  say "dT/dp | H = H_1/2" dTdp
   say "q1" q1
-  say "H'" h2 >> putStrLn ""
+  say "H_1" h2 >> putStrLn ""
   pure h2
 
 keplerIO :: IO [H Double]
 keplerIO = stepsIO n h hh0
   where
-    n = 3
-    h = pi / 240
+    n = 10
+    h = 0.001 -- pi / 240
 
 stepsIO :: (Show a, Floating a) => Int -> a -> H a -> IO [H a]
 stepsIO n h hh = iterateN n (stepIO h) hh
@@ -111,7 +128,7 @@ iterateN n f x0 = go 0 x0 []
 
 
 say :: Show a => String -> a -> IO ()
-say s x = putStrLn $ unwords [s, ":", show x]
+say s x = putStrLn $ unwords [s, ":\n  ", show x]
   
 
 

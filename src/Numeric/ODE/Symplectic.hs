62c5adee932bb65bc69cd1e651f5f5271699c99b
Marco Zocca <marco.zocca@unfoldml.com>
2022-03-13 10:54:56 +0100

    who put those NaNs in there

--- src/Numeric/ODE/Symplectic.hs
+++ src/Numeric/ODE/Symplectic.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# options_ghc -Wno-unused-imports #-}
+-- | Symplectic integration of ODEs
 module Numeric.ODE.Symplectic where
 
 import GHC.List (iterate')
@@ -17,6 +18,8 @@ instance Num a => Num (V a) where
   (*) = zipWithV (*)
   (-) = zipWithV (-)
   negate = fmap negate
+  abs = fmap abs
+  signum = fmap signum
 
 zipWithV :: (t -> t -> a) -> V t -> V t -> V a
 zipWithV f (V x1 y1) (V x2 y2) = V (f x1 y1) (f x2 y2)
@@ -29,7 +32,7 @@ norm = sqrt . normSq
 type P = V
 type Q = V
 
-data H a = H (V a) (V a) deriving (Eq, Show, Functor, Foldable, Traversable)
+data H a = H (P a) (Q a) deriving (Eq, Show, Functor, Foldable, Traversable)
 
 -- https://courses.seas.harvard.edu/courses/am225/notes/am225_symplectic.pdf
 hamiltonian :: Floating a => H a -> a
@@ -56,10 +59,51 @@ step h h0@(H p0 q0) = H p1 q1
   where
     (H _ q') = dH h0
     p1 = p0 - pure h * q'
-    h1 = H p1 q0
+    h1 = H p1 q0 -- half-step updated Hamiltonian
     (H p'' _) = dH h1
     q1 = q0 + pure h * p''
 
+stepIO :: (Show a, Floating a) => a -> H a -> IO (H a)
+stepIO h h0@(H p0 q0) = do
+  let
+    dh1@(H _ q') = dH h0
+    p1 = p0 - pure h * q'
+    h1 = H p1 q0 -- half-step updated Hamiltonian
+    dh2@(H p'' _) = dH h1
+    q1 = q0 + pure h * p''
+    h2 = H p1 q1
+  say "h0" h0
+  say "dh1" dh1
+  say "p1" p1
+  say "h1" h1
+  say "dh2" dh2
+  say "q1" q1
+  say "H'" h2 >> putStrLn ""
+  pure h2
+
+keplerIO :: IO [H Double]
+keplerIO = stepsIO n h hh0
+  where
+    n = 10
+    h = pi / 240
+
+stepsIO :: (Show a, Floating a) => Int -> a -> H a -> IO [H a]
+stepsIO n h hh = iterateN n (stepIO h) hh
+
+iterateN :: (Monad m) =>
+            Int -> (a -> m a) -> a -> m [a]
+iterateN n f x0 = go 0 x0 []
+  where
+    go i x acc
+      | i < n = do
+          x' <- f x
+          go (i + 1) x' (x' : acc)
+      | otherwise = pure acc
+
+
+say :: Show a => String -> a -> IO ()
+say s x = putStrLn $ unwords [s, ":", show x]
+  
 -- dHdt :: Floating a => H a -> (V a, V a)
 -- dHdt h = (- dq, dp)
 --   where
@@ -67,3 +111,7 @@ step h h0@(H p0 q0) = H p1 q1
 
 dH :: Floating b => H b -> H b
 dH = snd . grad hamiltonian
+
+
+ftest :: Floating a => [a] -> a
+ftest = recip . sum
